2144d1ef8b18f69bb009f7573637fa92c57c4b6f
OONISHI REON <66917548+Reon0429-cat@users.noreply.github.com>
2021-11-03 22:50:18 +0900

    props練習

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
+import ColorfulMessage from "./components/ColorfulMessage";
+
 // Reactでは関数を使って画面のコンポーネントを作る
 const App = () => {
   const onClickButton = () => alert();
-  // font-sizeをjsではキャメルケース(fontSize)にする
-  const contentStyle = {
-    color: "blue",
-    fontSize: "20px"
-  };
   return (
     // <> ... </>で囲わないといけない
     // ボタンを押されたときに処理を{}でjavascriptを書いていく
     <>
-      <h1 style={contentStyle}>こんにちは</h1>
-      <p>よろしく</p>
+      <h1>こんにちは</h1>
+      <ColorfulMessage color="blue" message="AAA" />
+      <ColorfulMessage color="red" message="BBB" />
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,14 @@
+const ColorfulMessage = (props) => {
+  // {color: "blue", message: "aaaa"}
+  console.log(props);
+  // 毎回props.colorのように書かなくて済む
+  const { color, message } = props;
+  // font-sizeをjsではキャメルケース(fontSize)にする
+  const contentStyle = {
+    color: color,
+    fontSize: "50px"
+  };
+  return <p style={contentStyle}>{message}</p>;
+};
+
+export default ColorfulMessage;
